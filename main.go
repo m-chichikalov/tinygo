b7234a37ae1d1ee60af32e0db640ee90d9d81f36
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-05-22 17:11:41 +0200

    main: add support for -tags flags

--- main.go
+++ main.go
@@ -53,6 +53,7 @@ type BuildConfig struct {
 	printSizes    string
 	cFlags        []string
 	ldFlags       []string
+	tags          string
 	wasmAbi       string
 }
 
@@ -90,6 +91,9 @@ func Compile(pkgName, outpath string, spec *TargetSpec, config *BuildConfig, act
 	}
 	for i := 1; i <= minor; i++ {
 		tags = append(tags, fmt.Sprintf("go1.%d", i))
+	}
+	if extraTags := strings.Fields(config.tags); len(extraTags) != 0 {
+		tags = append(tags, extraTags...)
 	}
 	compilerConfig := compiler.Config{
 		Triple:        spec.Triple,
@@ -554,6 +558,7 @@ func main() {
 	printIR := flag.Bool("printir", false, "print LLVM IR")
 	dumpSSA := flag.Bool("dumpssa", false, "dump internal Go SSA")
 	target := flag.String("target", "", "LLVM target")
+	tags := flag.String("tags", "", "a space-separated list of extra build tags")
 	printSize := flag.String("size", "", "print sizes (none, short, full)")
 	nodebug := flag.Bool("no-debug", false, "disable DWARF debug symbol generation")
 	ocdOutput := flag.Bool("ocd-output", false, "print OCD daemon output during debug")
@@ -578,6 +583,7 @@ func main() {
 		dumpSSA:       *dumpSSA,
 		debug:         !*nodebug,
 		printSizes:    *printSize,
+		tags:          *tags,
 		wasmAbi:       *wasmAbi,
 	}
 
